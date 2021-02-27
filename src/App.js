30d2ba4e981361a9e0fef0a9e86c35ce49c651ce
Raj Kale <kale44772@gmail.com>
2021-02-27 09:35:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {GoogleMap, withGoogleMap, withScriptjs,Marker,InfoWindow} from 'react-google-maps'
+import { useState,useEffect } from 'react';
+import firebase from './firebase'
+import mapStyles from './mapStyles'
+
+function Map()
+{
+  const [map,setMap]=useState(null);
+  const [data,setData]=useState("");
+
+  // Create Section
+  // const todoRef=firebase.database().ref("Products").child("Product1").child("Location");
+  // const todo={
+  //   lat:18.5204,
+  //   lng:73.8567
+  // }
+  // todoRef.push(todo);
+  
+    const todoRefe=firebase.database().ref("Products").child("Product1").child("Location");
+    console.log("HIIII")
+    todoRefe.on("value",(snapshot)=>{
+      setData(snapshot.val());
+      console.log(data);
+    })  
+
+
+ 
+
+  return <GoogleMap defaultZoom={10} defaultCenter={{lat:18.5204,lng:73.8567}}
+    defaultOptions={{style:mapStyles}}
+  >
+   {
+     data.map((data)=>(
+      <Marker
+      key={123}
+       position={{lat:data.lat,lng:data.lng}} 
+       onClick={()=>{setMap({lat:18.5204,lng:73.8567})}}
+       icon={{url:"https://static.thenounproject.com/png/429169-200.png",scaledSize:new window.google.maps.Size(45,45)}}/>
+     ))
+   }
+   
+  
+    {map && (
+      <InfoWindow 
+      position={{lat:map.lat,lng:map.lng}}
+      onCloseClick={()=>{
+        setMap(null)
+      }}>
+        <div>{map.lat}</div>
+      </InfoWindow>
+    )}
+  </GoogleMap>
+
+}
+
+const WrapperMap=withScriptjs(withGoogleMap(Map));
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <WrapperMap 
+       googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+       loadingElement={<div style={{ height: "100%" }}/>}
+       containerElement={<div style={{ height: `100%` }} />}
+       mapElement={<div style={{ height: `100%` }} />}
+       />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyCKNZPQPerFM92fruaWzKGnttkP4DrxAuc",
+    authDomain: "shipment-tracking-fe750.firebaseapp.com",
+    projectId: "shipment-tracking-fe750",
+    storageBucket: "shipment-tracking-fe750.appspot.com",
+    messagingSenderId: "901614304887",
+    appId: "1:901614304887:web:6df6f9f654d41e5eb65072",
+    measurementId: "G-2PYXTLCDS6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
